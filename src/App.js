5ca8792e91027975348aa7d2e6b3c7d587cf7a93
Hasan Deeb <h.deeb712@gmail.com>
2023-09-14 20:15:58 +0300

    displayed live forecast weather condition, with a describing icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Search from "./components/Search";
 import TodayWeather from "./components/TodayWeather.component";
 import WeatherItem from "./components/WeatherItem";
-import clear from "./img/weather-icons/clear.svg";
 import "./App.css";
 import { useState } from "react";
 import style from "./styles/weather-item.module.css";
+
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
@@ -13,7 +13,7 @@ const App = () => {
   let onInputChange = (e) => {
     setInputValue(e.target.value);
   };
-  console.log(inputValue);
+  
 
   let apiKey = "04eec21504ca03d13f534a27f6feb54c";
 
@@ -28,7 +28,10 @@ const App = () => {
           }
           return response.json();
         })
-        .then((country) => setWeatherData(country))
+        .then((country) => {
+          setWeatherData(country)
+          console.log(weatherData)
+        })
         .catch((error) => {
           setWeatherData(null);
         });
@@ -39,17 +42,26 @@ const App = () => {
     <div className="App">
       <main>
         <Search onInputChange={onInputChange} eventHandler={fetching} />
-        {weatherData ? (
+        {weatherData &&  (
+          <>
           <TodayWeather
             temp_min={Math.floor(weatherData.list[0].main.temp_min)}
             temp_max={Math.floor(weatherData.list[0].main.temp_max)}
             pressure={weatherData.list[0].main.pressure}
             humidity={weatherData.list[0].main.humidity}
             description={weatherData.list[0].weather[0].description}
-          />
-        ) : (
-          ""
-        )}
+            />
+
+            <section className={style.nextForecast}>
+            {weatherData.list.map((element,idx) =>{
+              if(idx === 0) return
+              console.log(element)
+              
+              return (<WeatherItem description={element.weather[0].description} key={element.cod} temp={Math.floor(element.main.temp)} time={(element.dt_txt).split(' ')[1].slice(0,-3)}/>)
+            })}
+            </section>
+            </>
+        ) }
         </main>
         </div>
   )
--- src/components/WeatherItem.jsx
+++ src/components/WeatherItem.jsx
@@ -1,12 +1,89 @@
 import {useState} from "react";
 import style from "../styles/weather-item.module.css"
-const WeatherItem = ({time, temp, src}) =>{
+import clear from "../img/weather-icons/clear.svg";
+import cloudy from "../img/weather-icons/cloudy.svg";
+import drizzle from "../img/weather-icons/drizzle.svg";
+import fog from "../img/weather-icons/fog.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../img/weather-icons/partlycloudy.svg";
+import rain from "../img/weather-icons/rain.svg";
+import snow from "../img/weather-icons/snow.svg";
+import storm from "../img/weather-icons/storm.svg";
+import unknown from "../img/weather-icons/unknown.svg";
+import styles from "../styles/ImgAndCaption.module.css";
 
+const WeatherItem = ({time, temp, src, description}) =>{
+    let condtitionImg = description.split(" ")[0]
+console.log(condtitionImg)
+    switch(condtitionImg){
+        case 'cloudy':
+            condtitionImg = cloudy
+            break;
+        case 'clear':
+            condtitionImg=clear
+            break;
+        case 'drizzle':
+            condtitionImg=drizzle
+            break;
+        case 'mist':
+            condtitionImg=fog
+            break;
+        case 'mostlycloudy':
+            condtitionImg=mostlycloudy
+            break;
+        case 'partlycloudy':
+            condtitionImg=partlycloudy
+            break;
+        case 'rain':
+            condtitionImg=rain
+            break;
+        case 'snow':
+            condtitionImg=snow
+            break;
+        case 'few':
+            condtitionImg=partlycloudy
+            break;
+        case 'scattered':
+            condtitionImg=cloudy
+            break;
+        case 'few':
+            condtitionImg=partlycloudy
+            break;
+        case 'shower':
+            condtitionImg=storm
+            break;
+        case 'thunderstorm':
+            condtitionImg=storm
+            break;
+        case 'thunderstorm':
+            condtitionImg=storm
+            break;
+        default:
+            condtitionImg=unknown        
+    }
+
+    // if (desc.split(" ")[0] == "clear") name = clear;
+    // else if (desc.split(" ")[0] == "cloudy"  desc.split(" ")[0] == "scattered")
+    //   name = cloudy;
+    // else if (desc.split(" ")[0] == "drizzle"  desc.split(" ")[0] == "broken")
+    //   name = drizzle;
+    // else if (desc.split(" ")[0] == "mist") name = fog;
+    // else if (desc.split(" ")[0] == "mostlycloudy") name = mostlycloudy;
+    // else if (desc.split(" ")[0] == "partlycloudy"  desc.split(" ")[0] == "few")
+    //   name = partlycloudy;
+    // else if (desc.split(" ")[0] == "rain") name = rain;
+    // else if (desc.split(" ")[0] == "snow") name = snow;
+    // else if (
+    //   desc.split(" ")[0] == "shower" 
+    //   desc.split(" ")[0] == "thunderstorm"
+    // )
+
+
+    
     return(
-     
         <section className={style.conditionContainer}>
         <i className="time">{time}</i>
-        <figure><img alt="clear-weather" width="100%" /></figure>
+        <figure><img src={condtitionImg} alt="clear-weather" width="100%" /></figure>
         <i className="temprature">{temp}&deg;C</i>
         </section>
     )
