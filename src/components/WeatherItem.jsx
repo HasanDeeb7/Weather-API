b47d64740bda6856a9330d99944012d015f3a251
Hasan Deeb <h.deeb712@gmail.com>
2023-09-14 20:22:47 +0300

    removed comments

--- src/components/WeatherItem.jsx
+++ src/components/WeatherItem.jsx
@@ -10,7 +10,6 @@ import rain from "../img/weather-icons/rain.svg";
 import snow from "../img/weather-icons/snow.svg";
 import storm from "../img/weather-icons/storm.svg";
 import unknown from "../img/weather-icons/unknown.svg";
-import styles from "../styles/ImgAndCaption.module.css";
 
 const WeatherItem = ({time, temp, src, description}) =>{
     let condtitionImg = description.split(" ")[0]
@@ -61,24 +60,6 @@ console.log(condtitionImg)
         default:
             condtitionImg=unknown        
     }
-
-    // if (desc.split(" ")[0] == "clear") name = clear;
-    // else if (desc.split(" ")[0] == "cloudy"  desc.split(" ")[0] == "scattered")
-    //   name = cloudy;
-    // else if (desc.split(" ")[0] == "drizzle"  desc.split(" ")[0] == "broken")
-    //   name = drizzle;
-    // else if (desc.split(" ")[0] == "mist") name = fog;
-    // else if (desc.split(" ")[0] == "mostlycloudy") name = mostlycloudy;
-    // else if (desc.split(" ")[0] == "partlycloudy"  desc.split(" ")[0] == "few")
-    //   name = partlycloudy;
-    // else if (desc.split(" ")[0] == "rain") name = rain;
-    // else if (desc.split(" ")[0] == "snow") name = snow;
-    // else if (
-    //   desc.split(" ")[0] == "shower" 
-    //   desc.split(" ")[0] == "thunderstorm"
-    // )
-
-
     
     return(
         <section className={style.conditionContainer}>
